a1dd90487cdce948c3d15a4a3892d25e6d1508ad
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-08-27 16:33:24 +0900

    テストデータの挿入

--- database/migrations/2020_08_27_052036_create_folders_table.php
+++ database/migrations/2020_08_27_052036_create_folders_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateFoldersTable extends Migration
 {
@@ -15,7 +15,7 @@ class CreateFoldersTable extends Migration
     {
         Schema::create('folders', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title, 20');
+            $table->string('title', 20);
             $table->timestamps();
         });
     }
---
+++ database/seeds/FoldersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\facades\DB;
+
+class FoldersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $titles = ['プライベート', 'タスク', '旅行'];
+
+        foreach ($titles as $title) {
+            DB::table('folders')->insert([
+                'title' => $title,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
+    }
+}

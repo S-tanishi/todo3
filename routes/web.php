5350b85345f9a227e923b5e2e88bb450f99aabc8
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-08-30 16:36:57 +0900

    ルーティング設定

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+
+Route::get('/folders/create', 'FolderController@cshowCreateForm')->name('folders.create');
+Route::post('folders/create', 'FolderController@create');

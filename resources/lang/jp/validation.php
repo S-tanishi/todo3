13d378daa959f206da35ff0aec7b0813b0b5c756
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-08-31 14:13:47 +0900

    文字数制限の追加

--- resources/lang/jp/validation.php
+++ resources/lang/jp/validation.php
@@ -111,7 +111,7 @@ return [
         'array' => 'The :attribute must contain :size items.',
     ],
     'starts_with' => 'The :attribute must start with one of the following: :values.',
-    'string' => 'The :attribute must be a string.',
+    'string' => ':attribute は :max 文字以内で入力してください',
     'timezone' => 'The :attribute must be a valid zone.',
     'unique' => 'The :attribute has already been taken.',
     'uploaded' => 'The :attribute failed to upload.',

03de2eb17952d6099ca197380294583d13179838
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-09-01 15:01:42 +0900

    ルーティングの設定

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 
 Route::get('/folders/create', 'FolderController@cshowCreateForm')->name('folders.create');
 Route::post('folders/create', 'FolderController@create');
+
+Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+Route:;post('/folders/{id}/tasks/create', 'TaskController@create');

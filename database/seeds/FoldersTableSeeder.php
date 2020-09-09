1a85b8761e9bd2463413db5a2789cc0851aafecf
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-09-09 09:25:29 +0900

    シーダーを再作成

--- database/seeds/FoldersTableSeeder.php
+++ database/seeds/FoldersTableSeeder.php
@@ -18,6 +18,7 @@ class FoldersTableSeeder extends Seeder
         foreach ($titles as $title) {
             DB::table('folders')->insert([
                 'title' => $title,
+                'user_id' => $user->id,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ]);
